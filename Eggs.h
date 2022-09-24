d3764ecb2edb67cb0cb1f94223724d776b84ffb8
c-cpp-a <111665869+c-cpp-a@users.noreply.github.com>
2022-09-24 13:19:26 +0800

    Add files via upload

---
+++ Eggs.h
@@ -0,0 +1,17 @@
+#ifndef EGGS_H_
+#define EGGS_H_
+//include STL
+//导入标准库 
+#include<string>
+//include custom library
+//导入自定义库
+#include "def"
+#include "People.h"
+class Egg{
+	private:
+	public:
+		int isegg(const int &,People &);
+		void check_run(const int &,People &);
+};
+#include "Eggs.cpp"
+#endif
--- main.cpp
+++ main.cpp
@@ -1,24 +1,28 @@
 //include STL
-//导入标准库 
-#include<iostream>
+//导入标准库
 #include<windows.h>
 //include custom library
 //导入自定义库 
+class Egg;
+class People;
+class Map; 
 #include "defines\def"
 #include "defines\init.h"
 #include "defines\Map.h"
 #include "defines\People.h"
+#include "defines\Eggs.h" 
 using namespace std;
 int main(){
 	Map G;
 	People p;
+	Egg egg; 
 	init(p,G);
 	while(1){
 		p.put_xy(G);
 		print_helper();
-		p.print_pos(); 
 		p.set_f(G);
 //		p.save(G);
+		egg.check_run(get_lang(),p);
 		Sleep(10);
 	}
 	return 0;
